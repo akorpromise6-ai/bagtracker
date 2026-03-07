febabae55012356dce135b34bd9aafe42fa1c9ca
Vincero <akorpromise6@gmail.com>
2026-03-07 16:40:54 +0100

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from “next/dynamic”;
-
-const BagTrackerApp = dynamic(() => import(”./BagTrackerApp”), { ssr: false });
-
-export default function Page() {
-return <BagTrackerApp />;
-}
