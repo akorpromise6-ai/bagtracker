1f5e45a5311b11f4e9fd8199aac9024cd4b31602
Vincero <akorpromise6@gmail.com>
2026-03-07 16:06:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from ‘next/dynamic’;
+import dynamic from “next/dynamic”;
 
-const BagTrackerApp = dynamic(() => import(’./BagTrackerApp’), { ssr: false });
+const BagTrackerApp = dynamic(() => import(”./BagTrackerApp”), { ssr: false });
 
 export default function Page() {
 return <BagTrackerApp />;
